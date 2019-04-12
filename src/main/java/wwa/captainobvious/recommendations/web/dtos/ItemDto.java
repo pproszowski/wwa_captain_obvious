340afd574bc0d8f4d133044617f84d0c7eb46337
Dariusz Szwarc <szw.dariusz@gmail.com>
2019-04-12 22:52:38 +0200

    Update DTOs to match json spec

--- src/main/java/wwa/captainobvious/recommendations/web/dtos/ItemDto.java
+++ src/main/java/wwa/captainobvious/recommendations/web/dtos/ItemDto.java
@@ -2,4 +2,10 @@ package wwa.captainobvious.recommendations.web.dtos;
 
 public class ItemDto {
 
+    public String id;
+    public String name;
+    public String categoryId;
+    public String imageUrl;
+    public int price;
+    public int count;
 }
--- src/main/java/wwa/captainobvious/recommendations/web/dtos/RecommendationDto.java
+++ src/main/java/wwa/captainobvious/recommendations/web/dtos/RecommendationDto.java
@@ -1,5 +1,8 @@
 package wwa.captainobvious.recommendations.web.dtos;
 
+import java.util.List;
+
 public class RecommendationDto {
 
+    public List<ItemDto> items;
 }
