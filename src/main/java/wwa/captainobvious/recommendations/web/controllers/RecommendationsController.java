d739d43c86a9ae6426d0919ac7570ebc0ae47e5c
Dariusz Szwarc <szw.dariusz@gmail.com>
2019-04-13 01:16:12 +0200

    Enable CORS for controllers

--- src/main/java/wwa/captainobvious/recommendations/web/controllers/RecommendationsController.java
+++ src/main/java/wwa/captainobvious/recommendations/web/controllers/RecommendationsController.java
@@ -1,5 +1,6 @@
 package wwa.captainobvious.recommendations.web.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/recommendation")
+@CrossOrigin(origins = "http://localhost:4200")
 public interface RecommendationsController {
 
     @GetMapping
