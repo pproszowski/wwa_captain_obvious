504c1f61fed751eed44c7d0b51aa476f75c389d8
Dariusz Szwarc <szw.dariusz@gmail.com>
2019-04-12 19:04:35 +0200

    Initialize project

---
+++ src/main/java/wwa/captainobvious/recommendations/RecommendationsApplication.java
@@ -0,0 +1,13 @@
+package wwa.captainobvious.recommendations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommendationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecommendationsApplication.class, args);
+    }
+
+}
