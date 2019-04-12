ff7cfcd85221e66aabffee67034c0724572408ad
Maciej.Fabicki <maciekfab@gmail.com>
2019-04-13 00:59:03 +0200

    product list small styling

--- src/main/webapp/src/app/product-list/product-list.component.ts
+++ src/main/webapp/src/app/product-list/product-list.component.ts
@@ -1,33 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
-class Product {
+class MockProduct {
 
-  name: string;
-  price: number;
-  image_url: string;
+    id: string;
+    name: string;
+    price: number;
+    count: number;
+    image_url: string;
 
-  constructor(name, price, image_url){
-    this.name = name;
-    this.price = price;
-    this.image_url = image_url;
-  }
+    constructor(name, price, count, image_url) {
+        this.id = "0";
+        this.name = name;
+        this.price = price;
+        this.image_url = image_url;
+        this.count = count;
+    }
 }
 
 @Component({
-  selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+    selector: 'app-product-list',
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
 
-  products: Product[];
+    products: MockProduct[];
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-    this.products = [];
-    this.products.push(new Product("Pendrive",30, "https://images-na.ssl-images-amazon.com/images/I/61fFVQlEB6L._SX425_.jpg"));
-    this.products.push(new Product("Peleryna",300, "https://sklepanimatora.pl/data/gfx/pictures/medium/2/7/10372_2.png"));
+    ngOnInit() {
+        this.products = [];
+        this.products.push(new MockProduct("Pendrive", 30, 2,  "https://images-na.ssl-images-amazon.com/images/I/61fFVQlEB6L._SX425_.jpg"));
+        this.products.push(new MockProduct("Peleryna", 300, 1, "https://sklepanimatora.pl/data/gfx/pictures/medium/2/7/10372_2.png"));
 
-  }
+    }
 }
