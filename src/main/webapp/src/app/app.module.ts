7ff1e6d18338afa8246c20a8c005028135fc9bef
Piotr Proszowski <p.proszowski@gmail.com>
2019-04-12 22:17:05 +0200

    Add material, boostrap, initialize recommendations component

--- src/main/webapp/src/app/app.module.ts
+++ src/main/webapp/src/app/app.module.ts
@@ -1,20 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ProductBasketComponent } from './product-basket/product-basket.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {ProductBasketComponent} from './product-basket/product-basket.component';
+import {RecommendationsComponent} from './product-basket/recommendations/recommendations.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterialModule} from '../material/material.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProductBasketComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ProductBasketComponent,
+        RecommendationsComponent,
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MaterialModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
