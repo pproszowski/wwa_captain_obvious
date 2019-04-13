010bf3f83b94ab6f3ad010ce349346eb7ec4df40
mcfb <maciekfab@gmail.com>
2019-04-13 06:42:55 +0200

    evaluator fix

--- src/main/java/wwa/captainobvious/recommendations/persistence/repos/Evaluator.java
+++ src/main/java/wwa/captainobvious/recommendations/persistence/repos/Evaluator.java
@@ -1,5 +1,23 @@
 package wwa.captainobvious.recommendations.persistence.repos;
 
+import wwa.captainobvious.recommendations.common.dtos.ItemDto;
+
 public class Evaluator {
 
+    public double evaluate(double cartValue, ItemDto product){
+
+        double highPriorityPriceWeight = 0.6;
+        double lowPriorityPriceWeight = 0.2;
+        double priceParam;
+        double priceWeight;
+
+        priceParam = (cartValue < product.price) ? cartValue / product.price : product.price /cartValue;
+        priceWeight = (cartValue < 40) ?  highPriorityPriceWeight :  lowPriorityPriceWeight;
+
+
+        double similarityWeight = (1 - priceWeight) / 2;
+        double otherBoughtWeight = (1 - priceWeight) / 2;
+
+        return priceWeight*priceParam + similarityWeight + otherBoughtWeight;
+    }
 }
