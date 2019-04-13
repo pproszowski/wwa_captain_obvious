4824777c81c348acfc3a2624404418d379123e64
Piotr Proszowski <p.proszowski@gmail.com>
2019-04-13 04:31:03 +0200

    Fetch data from backend

--- src/main/webapp/src/app/product-list/product-list.component.ts
+++ src/main/webapp/src/app/product-list/product-list.component.ts
@@ -1,38 +1,25 @@
-import {Component, OnInit} from '@angular/core';
-
-class MockProduct {
-
-    id: string;
-    name: string;
-    price: number;
-    count: number;
-    image_url: string;
-
-    constructor(name, price, count, image_url) {
-        this.id = "0";
-        this.name = name;
-        this.price = price;
-        this.image_url = image_url;
-        this.count = count;
-    }
-}
+import { Component, OnInit } from '@angular/core';
+import {ProductsProviderService} from '../products-provider/products-provider.service';
+import {Cart} from '../cart';
+import {Product} from '../product';
 
 @Component({
-    selector: 'app-product-list',
-    templateUrl: './product-list.component.html',
-    styleUrls: ['./product-list.component.css']
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
 
-    products: MockProduct[];
+    cart: Cart;
+    products: Product[];
 
-    constructor() {
-    }
+  constructor(private productsProvider: ProductsProviderService) { }
 
-    ngOnInit() {
-        this.products = [];
-        this.products.push(new MockProduct("Pendrive", 30, 2,  "https://images-na.ssl-images-amazon.com/images/I/61fFVQlEB6L._SX425_.jpg"));
-        this.products.push(new MockProduct("Peleryna", 300, 1, "https://sklepanimatora.pl/data/gfx/pictures/medium/2/7/10372_2.png"));
-
-    }
+  ngOnInit() {
+    this.productsProvider.getCart().subscribe(cart => {
+          this.cart = cart;
+          this.products = cart.items;
+        }
+    );
+  }
 }
--- src/main/webapp/src/app/products-provider/products-provider.service.ts
+++ src/main/webapp/src/app/products-provider/products-provider.service.ts
@@ -1,6 +1,7 @@
-import {Injectable} from '@angular/core';
+import {Injectable, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Product} from '../product';
+import {Observable} from 'rxjs';
 import {Cart} from '../cart';
 
 @Injectable({
@@ -8,39 +9,13 @@ import {Cart} from '../cart';
 })
 export class ProductsProviderService {
 
-    private cart: Cart;
     private baseUrl = 'http://localhost:8080/api/';
 
     constructor(private http: HttpClient) {
-        this.http.get<Cart>(this.baseUrl + 'cart').subscribe(cart =>{
-            this.cart = cart;
-            console.log(cart);
-        });
     }
 
-    getRecommendations() {
-        const ids = [];
-        this.cart.products.forEach(product => ids.push(product.id));
-        const idsString = ids.join(',');
-        let parameter = '?itemsIds=';
-        if (ids.length > 0) {
-            parameter += idsString;
-        } else {
-            parameter = '';
-        }
-        return this.http.get<Product[]>(this.baseUrl + 'recommendation' + parameter);
+    getCart(): Observable<Cart> {
+        return this.http.get<Cart>(this.baseUrl + 'cart');
     }
 
-    getRandomCartProducts() {
-        return this.http.get<Product[]>(this.baseUrl + 'products');
-    }
-
-    addProduct(product: Product) {
-        this.cart.products.push(product);
-    }
-
-    removeProduct(product: Product) {
-        const id = this.cart.products.indexOf(product);
-        this.cart.products.splice(id, 1);
-    }
 }
