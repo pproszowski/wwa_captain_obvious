a04f8e86ae09425f3953f78798d12fdaaf982ab5
mcfb <maciekfab@gmail.com>
2019-04-13 07:38:24 +0200

    Evaluator refactor

--- src/main/java/wwa/captainobvious/recommendations/persistence/repos/Evaluator.java
+++ src/main/java/wwa/captainobvious/recommendations/persistence/repos/Evaluator.java
@@ -4,16 +4,18 @@ import wwa.captainobvious.recommendations.common.dtos.ItemDto;
 
 public class Evaluator {
 
-    public double evaluate(double cartValue, ItemDto product){
+    static final double HIGH_PRIORITY_PRICE_WEIGHT = 0.6;
+    static final double LOW_PRIORITY_PRICE_WEIGHT = 0.2;
 
-        double highPriorityPriceWeight = 0.6;
-        double lowPriorityPriceWeight = 0.2;
+    private Evaluator() {}
+
+    public static double evaluate(double cartValue, ItemDto product){
+
         double priceParam;
         double priceWeight;
 
         priceParam = (cartValue < product.price) ? cartValue / product.price : product.price /cartValue;
-        priceWeight = (cartValue < 40) ?  highPriorityPriceWeight :  lowPriorityPriceWeight;
-
+        priceWeight = (cartValue < 40) ?  HIGH_PRIORITY_PRICE_WEIGHT :  LOW_PRIORITY_PRICE_WEIGHT;
 
         double similarityWeight = (1 - priceWeight) / 2;
         double otherBoughtWeight = (1 - priceWeight) / 2;
